b22bfe7123c9cff23b7cadca3a709c9f2eb29f48
CLRBLND99 <jan.throm@web.de>
2024-02-26 18:36:55 +0100

    removed unnecessary stuff

--- WebServicesBackend/HelperFunctions/HelperFunctions.cs
+++ WebServicesBackend/HelperFunctions/HelperFunctions.cs
@@ -8,30 +8,26 @@ namespace WebServicesBackend.HelperFunctions
         {
             var databaseThermostatService = new DatabaseThermostatService();
             var allThermostatIds = databaseThermostatService.GetAllThermostatIds();
-            if (allThermostatIds.Count() != 0)
-            {
+           
                 int? firstAvailable = Enumerable.Range(1, int.MaxValue)
                                 .Except(allThermostatIds)
                                 .FirstOrDefault();
 
                 return firstAvailable;
-            }
-            return 0;
+            
         }
         public static int? GetNextFreeRoomId()
         {
             var databaseRoomService = new DatabaseRoomService();
             var allRoomIds = databaseRoomService.GetAllRoomIds();
 
-            if (allRoomIds.Count() != 0)
-            {
+           
                 int? firstAvailable = Enumerable.Range(1, int.MaxValue)
                                 .Except(allRoomIds)
                                 .FirstOrDefault();
 
                 return firstAvailable;
-            }
-            return 0;
+            
         }
     }
 }
