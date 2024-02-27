630c0ea390ad57a9e7e207706362f92555122c54
MassivNikki <nickls.ply@gmail.com>
2024-02-27 14:18:02 +0100

    knopf für das hinzufügen des raumes(erstmal nur logik wird noch verschönert)

--- WebServicesFrontend/src/app/app.component.ts
+++ WebServicesFrontend/src/app/app.component.ts
@@ -18,11 +18,50 @@ export class AppComponent implements OnInit {
   httpClient = inject(HttpClient);
   rooms:any = [];
   fetchAllRooms():void{
-      this.httpClient.get('https://localhost:32772/GetAllRooms').subscribe((data:any)=>{
+      this.httpClient.get('https://localhost:32770/GetAllRooms').subscribe((data:any)=>{
         this.rooms = data;
         console.log(this.rooms);
       });
   }
+
+  openAddWindow(){
+    console.log("test");
+    var body = document.body;
+    
+    // Erstelle ein neues Textfeld
+    var textfeld = document.createElement("input");
+    textfeld.type = "text";
+    textfeld.id = "nameField";
+    
+    // Erstelle einen neuen Button
+    var button = document.createElement("button");
+    button.innerHTML = "Hinzufügen!";
+    button.addEventListener('click', () => {
+      this.addRoom(textfeld.value);
+    });
+    
+    // Füge das Textfeld und den Button dem Body des Dokuments hinzu
+    body.appendChild(textfeld);
+    body.appendChild(button);
+    // [].forEach.call(document.querySelectorAll('main'),
+    // function (e) {
+      
+    //     e.innerHTML = e.innerHTML +//the user fills the fields, which then will be used by the php code
+    //         "<div class='addWindow'><div class='addForm'>"+
+    //         "Name<br><input class='textFieldAddW' type='text' name='topicName' id='nameField' required><br>" +
+    //         //confirms the action
+    //         "<button name='Add' (click)=addRoom()>Add Topic</button>" +
+    //         //cancels the action
+    //         "<button class='cancelBtt' type='submit' name='close' onclick='closeWindow(\"topicNameField\")'>Cancel</button></div></div>";
+    // });
+}
+addRoom(name):void{
+    this.httpClient.post<any>("https://localhost:32770/AddRoom?roomName="+name,null).subscribe(response => {
+      console.log(response); // Hier erhältst du die Antwort von der API
+      // Füge hier die Logik hinzu, die du nach dem Hinzufügen des Raums ausführen möchtest
+    });
+    console.log(name);
+}
 
   ngOnInit():void{
     this.fetchAllRooms();
